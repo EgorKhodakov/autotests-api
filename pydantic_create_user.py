91ff39c617d380bfc69d78a0b768dde2611588ce
EgorKhodakov <khodakov91@yandex.ru>
2026-01-04 01:18:18 +0300

    Знакомство с Pydantic

---
+++ pydantic_create_user.py
@@ -0,0 +1,31 @@
+import uuid
+from pydantic import BaseModel, Field, EmailStr
+from tools.fakers import random_email, random_password
+
+
+class UserSchema(BaseModel):
+    """
+    Модель юзера
+    """
+    id: str = Field(default_factory=lambda: str(uuid.uuid4()))
+    email: EmailStr = Field(default_factory=random_email)
+    last_name: str = Field(alias="lastName", default="Khodakov")
+    first_name: str = Field(alias="firstName", default="Egor")
+    middle_name: str = Field(alias="middleName", default="Ivanovich")
+
+
+class CreateUserRequestSchema(BaseModel):
+    """
+    Модель запроса на создание юзера
+    """
+    email: EmailStr = Field(default_factory=random_email)
+    password: str = Field(default_factory=random_password)
+    last_name: str = Field(alias="lastName", default="Khodakov")
+    first_name: str = Field(alias="firstName", default="Egor")
+    middle_name: str = Field(alias="middleName", default="Ivanovich")
+
+class CreateUserResponseSchema(BaseModel):
+    """
+    Модель ответа на создание пользователя
+    """
+    user: UserSchema
