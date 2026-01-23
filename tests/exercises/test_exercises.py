6030717dae85f8b94f876d1efdfcf57b95a0243a
EgorKhodakov <khodakov91@yandex.ru>
2026-01-23 15:12:18 +0300

    Добавлен тест test_get_exercise

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -3,10 +3,12 @@ from http import HTTPStatus
 import pytest
 
 from clients.exercises.exercises_client import ExercisesClient
-from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
+    GetExerciseResponseSchema
 from fixtures.courses import CoursesFixture
+from fixtures.exercises import ExersiceFixture
 from tools.assertions.base import assert_status_code
-from tools.assertions.exercises import assert_create_exercise
+from tools.assertions.exercises import assert_create_exercise, assert_get_exercise
 from tools.assertions.schema import validate_json_schema
 
 
@@ -24,3 +26,15 @@ class TestExercises:
         assert_status_code(response.status_code, HTTPStatus.OK)
         validate_json_schema(response.json(), response_data.model_json_schema())
         assert_create_exercise(response_data, request)
+
+
+    def test_get_exercise(self, exercises_client: ExercisesClient, function_exercise: ExersiceFixture) -> None:
+
+        response = exercises_client.get_exercise_api(exercise_id=function_exercise.response.exercise.id)
+        response_data = GetExerciseResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        validate_json_schema(response.json(), response_data.model_json_schema())
+        assert_get_exercise(function_exercise.response, response_data)
+
+
--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -1,4 +1,5 @@
-from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
+    ExerciseSchema, GetExerciseResponseSchema
 from tools.assertions.base import assert_equal
 
 
@@ -16,3 +17,30 @@ def assert_create_exercise(actual: CreateExerciseResponseSchema, expected: Creat
     assert_equal(actual.exercise.order_index, expected.order_index, "order_index")
     assert_equal(actual.exercise.description, expected.description, "description")
     assert_equal(actual.exercise.estimated_time, expected.estimated_time, "estimated_time")
+
+
+def assert_exercise(actual: ExerciseSchema, expected: ExerciseSchema):
+    """
+    Сравнение данных двух моделей упражнения
+    :param actual: Полученный от API ответ
+    :param expected: Ожидаемый от API ответ
+    :return: AssertionError если хотя бы одно значение не совпадает
+    """
+    assert_equal(actual.id, expected.id, "id")
+    assert_equal(actual.title, expected.title, "title")
+    assert_equal(actual.course_id, expected.course_id, "course_id")
+    assert_equal(actual.max_score, expected.max_score, "max_score")
+    assert_equal(actual.min_score, expected.min_score, "min_score")
+    assert_equal(actual.order_index, expected.order_index, "order_index")
+    assert_equal(actual.description, expected.description, "description")
+    assert_equal(actual.estimated_time, expected.estimated_time, "estimated_time")
+
+
+def assert_get_exercise(actual: CreateExerciseResponseSchema, expected: GetExerciseResponseSchema):
+    """
+    Проверка ответа полученного от API на запрос получения упражнения
+    :param actual: Ответ полученный от API при создании упражнения
+    :param expected: Ответ полученный от API при запросе упражнения по id
+    :return: AssertionError если хотя бы одно значение не совпадает
+    """
+    assert_exercise(actual.exercise, expected.exercise)
